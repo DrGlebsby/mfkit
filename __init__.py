2dd553c20dca646a7c94ae9b7309dfd4e72d4489
Gleb Averochkin <glebaver@gmail.com>
2026-06-02 17:24:50 +0300

    Delete __init__.py

--- __init__.py
+++
@@ -1,8 +0,0 @@
-from .featurizer import all_chosen_features, featurize, featurize_to_dict, featurize_many
-
-__all__ = [
-    "all_chosen_features",
-    "featurize",
-    "featurize_to_dict",
-    "featurize_many",
-]
